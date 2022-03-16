f78edae181df88b906a6d01dfad0c73d4329ec05
bveha <bveha@DESKTOP-S87GG1J>
2022-03-16 16:54:22 +0100

    Aggiungere i file di progetto.

---
+++ AS2122_3G_INF_BunardziuVehap_Pizzeria/Form1.cs
@@ -0,0 +1,10 @@
+namespace AS2122_3G_INF_BunardziuVehap_Pizzeria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
